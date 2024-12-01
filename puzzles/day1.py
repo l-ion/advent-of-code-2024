d7289ef3079a8d171edd80b98092fa2ac6bc764b
Lau Ionele <laurentiu.ionele@gmail.com>
2024-12-01 05:33:29 +0000

    Fix typos

--- puzzles/day1.py
+++ puzzles/day1.py
@@ -1,7 +1,6 @@
-def solve(input_file):
+def solvePart1(input_file):
     with open(input_file, 'r') as f:
         data = [line.strip() for line in f.readlines()]
-    # Your puzzle-solving logic here
     left_numbers = []
     right_numbers = []
     for line in data:
@@ -18,26 +17,25 @@ def solve(input_file):
 def solvePart2(input_file):
     with open(input_file, 'r') as f:
         data = [line.strip() for line in f.readlines()]
-    # Your puzzle-solving logic here
     left_numbers = []
     right_numbers = []
     for line in data:
         left, right = map(int, line.split())
         left_numbers.append(left)
         right_numbers.append(right)
-    score = 0
-    occurences  = {}
+    similarityScore = 0
+    occurrences = {}
     for key in set(left_numbers):
-        occurences[key] = right_numbers.count(key)
+        occurrences[key] = right_numbers.count(key)
 
     for number in left_numbers:
-        score += occurences[number] * number
+        similarityScore += occurrences[number] * number
 
-    return score
+    return similarityScore
 
 if __name__ == "__main__":
     day = "day1"  # Change this for each day
     input_file = f'../input/{day}.txt'
-    print(f"{day.capitalize()} Solution:", solve(input_file))
-    print(f"{day.capitalize()} Solution:", solvePart2(input_file))
+    print(f"{day.capitalize()} part 1 solution:", solvePart1(input_file))
+    print(f"{day.capitalize()} part 2 Solution:", solvePart2(input_file))
 
